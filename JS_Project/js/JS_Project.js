6ddb5961650776b9c63495e9ae501e849476c895
EliasMontiel <elias.ignacio.2004@gmail.com>
2024-11-21 13:42:48 -0700

    js project progress

--- JS_Project/js/JS_Project.js
+++ JS_Project/js/JS_Project.js
@@ -1,8 +1,11 @@
+let proj;
+
 fetch('/JS_Project/projects.json')
     .then(response => {
         return response.json();
     }).then(projects => {
         console.log(projects);
+        proj = projects;
         parseData(projects);
     }).catch(err => {
         console.log(`error ${err}`);
@@ -10,12 +13,39 @@ fetch('/JS_Project/projects.json')
 
 function parseData(data){
     for(let i=0; i<data.projects.length; i++){
-       document.getElementById("projects").innerHTML += 
-       `<div class="row project" id="${data.projects[i].subdomain}>"
-       <div class="proj_img"><img src="images/.........  (${i + 1}).png"></div>
-       <div class="description"><h2>${projects.project[i].name}</h2><p class="subtitle">${data.projects[i].subtitle}</p>
-       <p>${data.projects[i].abstract}</p></div></div>`;
-       data.projects[i].name;
+        document.getElementById("projects").innerHTML += 
+        `<a href="/JS_Project/${data.projects[i].subdomain}.html">
+        <div class="row project" id="${data.projects[i].subdomain}">
+        <div class="projimg"><img src="${data.projects[i].images}"></div>
+        <div class="description"><h2>${data.projects[i].name}</h2>
+        <p class="subtitle">${data.projects[i].subtitle}</p>
+        <p>${data.projects[i].abstract}</p></div></div></a>`;
+        }
 
+}
+
+for(b of document.querySelectorAll("#buttons button")){
+    b.addEventListener("click", e=>{
+        console.log(e.target.value);
+        sortProjects(e.target.value);
+    })
+}
+
+function sortProjects(button){
+    if(button == "clear"){
+        for(let i=0; i<proj.projects.length; i++){
+            document.getElementById(proj.projects[i].subdomain).style.display = "flex";
+        }
+    }else if(button != undefined){
+        for(let i=0; i<proj.projects.length;i++){
+            if(proj.projects[i].category.includes(button) == true){
+                document.getElementById(proj.projects[i].subdomain).style.display = "flex";
+            }else{
+                document.getElementById(proj.projects[i].subdomain).style.display = "none";
+            }
+        }
+    }else{
+        console.log("error, button value is undefined");
     }
+
 }
